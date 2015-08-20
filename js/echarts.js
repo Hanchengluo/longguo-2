446d4acb9e849c73f35c9202230384a4c4e40934
huang2a <huang2a>
2015-08-20 19:26:30 +0800

    add content-me add help

--- js/echarts.js
+++ js/echarts.js
@@ -2,7 +2,7 @@
     var myChart01 = echarts.init(document.getElementById('chart01'));
     var option = {
              title: {
-                 text: "",
+
                  subtext: "",
                  textStyle: {
                      fontSize: 14,
@@ -15,8 +15,8 @@
              legend: {
                  data: ["最高价格", "最低价格"],
                  selectedMode: false,
-                 x: "300px",
-                 y: "20px"
+                 x: "right",
+                 y: "top"
              },
              toolbox: {
                  feature: {
@@ -68,7 +68,6 @@
     var myChart02 = echarts.init(document.getElementById('chart02'));
     var option = {
         title: {
-           text: "",
            subtext: "",
            textStyle: {
                fontSize: 14,
@@ -81,8 +80,8 @@
        legend: {
            data: ["最高价格", "最低价格"],
            selectedMode: false,
-           x: "300px",
-           y: "20px"
+           x: "right",
+           y: "top"
        },
        toolbox: {
            feature: {
@@ -140,7 +139,6 @@
     var myChart03 = echarts.init(document.getElementById('chart03'));
     var option = {
         title: {
-           text: "",
            subtext: "",
            textStyle: {
                fontSize: 14,
@@ -153,8 +151,8 @@
        legend: {
            data: ["最高价格", "最低价格"],
            selectedMode: false,
-           x: "300px",
-           y: "20px"
+           x: "right",
+           y: "top"
        },
        toolbox: {
            feature: {
@@ -214,7 +212,6 @@
     var myChart04 = echarts.init(document.getElementById('chart04'));
     var option = {
         title: {
-           text: "六月苯酐价格变化指数",
            subtext: "",
            textStyle: {
                fontSize: 14,
@@ -288,7 +285,6 @@
     var myChart05 = echarts.init(document.getElementById('chart05'));
     var option = {
         title: {
-           text: "六月苯酐价格变化指数",
            subtext: "",
            textStyle: {
                fontSize: 14,
@@ -361,7 +357,6 @@
     var myChart06 = echarts.init(document.getElementById('chart06'));
     var option = {
         title: {
-           text: "六月苯酐价格变化指数",
            subtext: "",
            textStyle: {
                fontSize: 14,
@@ -434,7 +429,6 @@
     var myChart07 = echarts.init(document.getElementById('chart07'));
     var option = {
         title: {
-           text: "六月苯酐价格变化指数",
            subtext: "",
            textStyle: {
                fontSize: 14,
@@ -503,148 +497,3 @@
     // 为echarts对象加载数据
     myChart07.setOption(option);
 
-    //charts08
-    var myChart08 = echarts.init(document.getElementById('chart08'));
-    var option = {
-        title: {
-           text: "六月苯酐价格变化指数",
-           subtext: "",
-           textStyle: {
-               fontSize: 14,
-               fontStyle: "normal"
-           }
-       },
-       tooltip: {
-           trigger: "axis"
-       },
-       legend: {
-           data: ["最高价格", "最低价格"],
-           selectedMode: false,
-           x: "right",
-           y: "top"
-       },
-       toolbox: {
-           feature: {
-               dataView: {
-                   readOnly: true
-               },
-               magicType: {
-                   type: ["line", "bar"],
-                   show: false
-               }
-           }
-       },
-       xAxis: [
-           {
-               type: "category",
-               boundaryGap: false,
-               name:"(时间)",
-               data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-               name: "",
-               nameLocation: "end",
-               nameTextStyle:{
-                    color: "rgb(102, 103, 104)",
-                    align: "right"
-               }
-           }
-       ],
-       yAxis: [
-           {
-               type: "value",
-               name: "（元/吨）",
-               position: "top",
-               nameLocation: "end",
-               nameTextStyle: {
-                   color: "rgb(102, 103, 104)",
-                   align: "right"
-               }
-           }
-       ],
-       series: [
-           {
-               name: "最高价格",
-               type: "line",
-               data: [7838, 6040, 8960, 7328, 7603, 8372,9010]
-           },
-           {
-               name: "最低价格",
-               type: "line",
-               data: [4900, 4039, 6228, 5938, 7030, 8072,9083]
-           }
-       ]
-    };
-    // 为echarts对象加载数据
-    myChart08.setOption(option);
-
-    //chart09
-    var myChart09 = echarts.init(document.getElementById('chart09'));
-    var option = {
-        title: {
-           text: "六月苯酐价格变化指数",
-           subtext: "",
-           textStyle: {
-               fontSize: 14,
-               fontStyle: "normal"
-           }
-       },
-       tooltip: {
-           trigger: "axis"
-       },
-       legend: {
-           data: ["最高价格", "最低价格"],
-           selectedMode: false,
-           x: "right",
-           y: "top"
-       },
-       toolbox: {
-           feature: {
-               dataView: {
-                   readOnly: true
-               },
-               magicType: {
-                   type: ["line", "bar"],
-                   show: false
-               }
-           }
-       },
-       xAxis: [
-           {
-               type: "category",
-               boundaryGap: false,
-               name:"(时间)",
-               data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-               name: "",
-               nameLocation: "end",
-               nameTextStyle:{
-                    color: "rgb(102, 103, 104)",
-                    align: "right"
-               }
-           }
-       ],
-       yAxis: [
-           {
-               type: "value",
-               name: "（元/吨）",
-               position: "top",
-               nameLocation: "end",
-               nameTextStyle: {
-                   color: "rgb(102, 103, 104)",
-                   align: "right"
-               }
-           }
-       ],
-       series: [
-           {
-               name: "最高价格",
-               type: "line",
-               data: [7838, 6040, 8060, 7028, 7003, 8372,9310]
-           },
-           {
-               name: "最低价格",
-               type: "line",
-               data: [4929, 4039, 6228, 5038, 7030, 8872,9283]
-           }
-       ]
-    };
-    // 为echarts对象加载数据
-    myChart09.setOption(option);
